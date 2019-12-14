497f31e2568502af0ff1f0821b1cf966156e1403
Chrossu <Chrossu@users.noreply.github.com>
2019-12-13 22:07:39 -0300

    Redux functionality refactored

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux'
-import todos from './todos'
+import todos from './todos/todos'
 import visibilityFilter from './visibilityFilter'
 
 export default combineReducers({
