95cbe928ab3c86bb89aff1babdcc380cc9ba31ed
Chrossu <Chrossu@users.noreply.github.com>
2019-12-13 22:01:55 -0300

    Refactored the way Redux store is implemented

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
-import rootReducer from './reducers'
 
-const store = createStore(rootReducer)
+import store from './redux/store';
 
 render(
   <Provider store={store}>
